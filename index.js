cda7e9e99beec9c80e6a418bb64dbd17bcdf7c2c
Jordan Martin <jdordan23@gmail.com>
2018-08-10 04:05:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ function happyHolidays() {
     return "Happy Holidays!"
 }
 
-function happyHOlidaysTo(string) {
-    return
+function happyHolidaysTo(name) {
+    return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name) {
+  return 
 }
